856d11513f8cdd98c6019d14575159250c80f77a
nimeshaviduranga <49368109+nimeshaviduranga@users.noreply.github.com>
2025-05-08 12:28:11 +0530

    Create Task model

---
+++ backend/src/main/java/com/todo/task/backend/model/Task.java
@@ -0,0 +1,38 @@
+package com.todo.task.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "task")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+}
